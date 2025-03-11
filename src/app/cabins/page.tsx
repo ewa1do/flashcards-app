1e3cab7e36de764df4e3b5256b34498e00d7d7d8
Eduardo Vera <this.eduardovera@gmail.com>
2025-03-10 22:09:21 -0400

    Overview nextjs

--- src/app/cabins/page.tsx
+++ src/app/cabins/page.tsx
@@ -1,3 +1,22 @@
-export default function page() {
-  return <div>page</div>;
+import Counter from "../components/Counter";
+
+export default async function page() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
+    console.log(data);
+
+    return (
+        <div>
+            <h1>Cabins:</h1>
+
+            <ul>
+                {data.map((user: { id: number; name: string }) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+
+            <Counter />
+        </div>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <Navigation />
-      <h1>The Wild Oasis. Welcome to paradise.</h1>;
-      <Link href="/cabins">Explore luxury cabins</Link>;
-    </div>
-  );
+    return (
+        <div>
+            <h1>The Wild Oasis. Welcome to paradise.</h1>;
+            <Link href="/cabins">Explore luxury cabins</Link>;
+        </div>
+    );
 }
